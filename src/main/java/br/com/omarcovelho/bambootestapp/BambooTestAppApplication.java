f648b0c7bec3723cf0bb84284f3dcaed1c3db437
Marco Prado <marco.prado@gsw.com.br>
2021-03-29 15:05:48 -0300

    Initial commit

---
+++ src/main/java/br/com/omarcovelho/bambootestapp/BambooTestAppApplication.java
@@ -0,0 +1,13 @@
+package br.com.omarcovelho.bambootestapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BambooTestAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BambooTestAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/omarcovelho/bambootestapp/BambooTestAppApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.omarcovelho.bambootestapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BambooTestAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
